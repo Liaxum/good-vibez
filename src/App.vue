3a7f1b7a3653fab3dcc781d98a3f9a2f9beb69b9
Liaxum <texier.r@gmail.com>
2022-12-06 01:01:13 +0100

    @upd: resphere

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,40 @@
 <template>
-  <h1>Coming Soon Good vibes!</h1>
+  <Renderer ref="renderer" :orbitCtrl="{autoRotate: true}" resize="window">
+    <Camera :position="{ z: 6}"/>
+    <Scene>
+      <PointLight color="white" :position="{z: 50}"/>
+      <PointLight color="white" :position="{z: -50}"/>
+      <PointLight color="white" :position="{x: 50}"/>
+      <PointLight color="white" :position="{x: -50}"/>
+
+      <Sphere ref="mesh" :radius="2.5" :width-segments="500" :height-segments="500" >
+        <StandardMaterial :props="{displacementScale: 0.1}">
+          <Texture src="/worldColour.5400x2700.jpg" />
+          <Texture src="/srtm_ramp2.world.5400x2700.jpg" name="displacementMap" />
+        </StandardMaterial>
+      </Sphere>
+    </Scene>
+  </Renderer>
 </template>
 <script>
 document.title = 'Good vibez';
+
+export default {
+  mounted() {
+    const renderer = this.$refs.renderer;
+    const box = this.$refs.box;
+
+    renderer.onBeforeRender(() => {
+    })
+  },
+  data: () => ({
+    width: window.innerWidth,
+    heiht: window.innerHeight
+  }),
+}
 </script>
+<style>
+body, html {
+  margin: 0;
+}
+</style>
